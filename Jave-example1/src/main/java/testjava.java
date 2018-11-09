0292f4140c53a94373ee01de8618dfaa5a6acc70
Ashis Kumar Panda <ashpanda@publicisgroupe.net>
2018-11-09 18:04:19 +0530

    First commit

---
+++ Jave-example1/src/main/java/testjava.java
@@ -0,0 +1,15 @@
+public class testjava {
+    public static void main(String args[]){
+
+
+        int num1 = 10;
+        int num2 = 20;
+
+        int total = num1+num2;
+
+        System.out.println(total);
+
+        System.out.println("Sum of Two no is"+total);
+
+    }
+}
